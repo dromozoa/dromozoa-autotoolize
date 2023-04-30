0b65c34e66386aa75c1198d5e832efe1c94b3e75
Tomoyuki Fujimori <moyu@dromozoa.com>
2023-04-30 14:55:08 +0900

    著作権表記を更新。

--- build.sh
+++ build.sh
@@ -11,11 +11,11 @@
 #
 # dromozoa-autotoolize is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with dromozoa-autotoolize.  If not, see <http://www.gnu.org/licenses/>.
+# along with dromozoa-autotoolize. If not, see <https://www.gnu.org/licenses/>.
 
 here=`pwd`
 
